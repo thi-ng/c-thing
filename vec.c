961b0333ce782e62c5cda84bb2da34f95373b3fe
Karsten Schmidt <k@postspectacular.com>
2016-05-09 00:43:02 +0100

    add VECOP macros for Vec2, implement for all 4 math ops

--- vec.c
+++ vec.c
@@ -21,53 +21,30 @@ CT_Vec2f *ct_set2fxy(CT_Vec2f *v, float x, float y) {
   return v;
 }
 
-CT_Vec2f *ct_add2fv(CT_Vec2f *a, CT_Vec2f *b, CT_MPool* mpool) {
-  CT_Vec2f *v = ALLOCATE_TYPE(mpool, CT_Vec2f);
-  if (v != NULL) {
-    v->x = a->x + b->x;
-    v->y = a->y + b->y;
-  }
-  return v;
-}
+VEC2OP(CT_Vec2f, float, ct_add2f, ct_set2f, +)
 
-CT_Vec2f *ct_add2fv_imm(CT_Vec2f *a, CT_Vec2f *b) {
-  a->x += b->x;
-  a->y += b->y;
-  return a;
-}
+VEC2OP(CT_Vec2f, float, ct_sub2f, ct_set2f, -)
 
-CT_Vec2f *ct_add2fn(CT_Vec2f *a, float n, CT_MPool* mpool) {
-  CT_Vec2f *v = ALLOCATE_TYPE(mpool, CT_Vec2f);
-  if (v != NULL) {
-    v->x = a->x + n;
-    v->y = a->y + n;
-  }
-  return v;
-}
+VEC2OP(CT_Vec2f, float, ct_mul2f, ct_set2f, *)
 
-CT_Vec2f *ct_add2fn_imm(CT_Vec2f *v, float n) {
-  v->x += n;
-  v->y += n;
-  return v;
-}
-
-CT_Vec2f *ct_add2fxy(CT_Vec2f *a, float x, float y, CT_MPool* mpool) {
-  CT_Vec2f *v = ALLOCATE_TYPE(mpool, CT_Vec2f);
-  if (v != NULL) {
-    v->x = a->x + x;
-    v->y = a->y + y;
-  }
-  return v;
-}
-
-CT_Vec2f *ct_add2fxy_imm(CT_Vec2f *v, float x, float y) {
-  v->x += x;
-  v->y += y;
-  return v;
-}
+VEC2OP(CT_Vec2f, float, ct_div2f, ct_set2f, /)
 
 float ct_dot2fv(CT_Vec2f *a, CT_Vec2f *b) {
   return a->x * b->x + a->y * b->y;
+}
+
+CT_Vec2f *ct_normalize2f_imm(CT_Vec2f *v, float len) {
+  float m = v->x * v->x + v->y * v->y;
+  if (m > 0.0) {
+    len /= m;
+    v->x *= len;
+    v->y *= len;
+  }
+  return v;
+}
+
+CT_Vec2f *ct_normalize2f(CT_Vec2f *v, float len, CT_MPool *mpool) {
+  return ct_normalize2f_imm(ct_set2fv(ALLOCATE_TYPE(mpool, CT_Vec2f), v), len);
 }
 
 ////// Vec3f
@@ -96,16 +73,6 @@ CT_Vec3f *ct_set3fxyz(CT_Vec3f *v, float x, float y, float z) {
   return v;
 }
 
-CT_Vec3f *ct_add3fv(CT_Vec3f *a, CT_Vec3f *b, CT_MPool* mpool) {
-  CT_Vec3f *v = ALLOCATE_TYPE(mpool, CT_Vec3f);
-  if (v != NULL) {
-    v->x = a->x + b->x;
-    v->y = a->y + b->y;
-    v->z = a->z + b->z;
-  }
-  return v;
-}
-
 CT_Vec3f *ct_add3fv_imm(CT_Vec3f *a, CT_Vec3f *b) {
   a->x += b->x;
   a->y += b->y;
@@ -113,14 +80,8 @@ CT_Vec3f *ct_add3fv_imm(CT_Vec3f *a, CT_Vec3f *b) {
   return a;
 }
 
-CT_Vec3f *ct_add3fn(CT_Vec3f *a, float n, CT_MPool* mpool) {
-  CT_Vec3f *v = ALLOCATE_TYPE(mpool, CT_Vec3f);
-  if (v != NULL) {
-    v->x = a->x + n;
-    v->y = a->y + n;
-    v->z = a->z + n;
-  }
-  return v;
+CT_Vec3f *ct_add3fv(CT_Vec3f *a, CT_Vec3f *b, CT_MPool* mpool) {
+  return ct_add3fv_imm(ct_set3fv(ALLOCATE_TYPE(mpool, CT_Vec3f), a), b);
 }
 
 CT_Vec3f *ct_add3fn_imm(CT_Vec3f *v, float n) {
@@ -130,33 +91,53 @@ CT_Vec3f *ct_add3fn_imm(CT_Vec3f *v, float n) {
   return v;
 }
 
-CT_Vec3f *ct_add3fxy(CT_Vec3f *a, float x, float y, float z, CT_MPool* mpool) {
-  CT_Vec3f *v = ALLOCATE_TYPE(mpool, CT_Vec3f);
-  if (v != NULL) {
-    v->x = a->x + x;
-    v->y = a->y + y;
-    v->z = a->z + z;
-  }
-  return v;
+CT_Vec3f *ct_add3fn(CT_Vec3f *a, float n, CT_MPool* mpool) {
+  return ct_add3fn_imm(ct_set3fv(ALLOCATE_TYPE(mpool, CT_Vec3f), a), n);
 }
 
-CT_Vec3f *ct_add3fxy_imm(CT_Vec3f *v, float x, float y, float z) {
+CT_Vec3f *ct_add3fxyz_imm(CT_Vec3f *v, float x, float y, float z) {
   v->x += x;
   v->y += y;
   v->z += z;
   return v;
 }
 
+CT_Vec3f *ct_add3fxyz(CT_Vec3f *a, float x, float y, float z, CT_MPool* mpool) {
+  return ct_add3fxyz_imm(ct_set3fv(ALLOCATE_TYPE(mpool, CT_Vec3f), a), x, y, z);
+}
+
 float ct_dot3fv(CT_Vec3f *a, CT_Vec3f *b) {
   return a->x * b->x + a->y * b->y + a->z * b->z;
 }
 
+CT_Vec3f *ct_cross3fv_imm(CT_Vec3f *a, CT_Vec3f *b) {
+  float x = a->y * b->z - a->z * b->y;
+  float y = a->z * b->x - a->x * b->z;
+  a->z = a->x * b->y - a->y * b->x;
+  a->y = y;
+  a->x = x;
+  return a;
+}
+
 CT_Vec3f *ct_cross3fv(CT_Vec3f *a, CT_Vec3f *b, CT_MPool* mpool) {
   CT_Vec3f *v = ALLOCATE_TYPE(mpool, CT_Vec3f);
-  if (v != NULL) {
-    v->x = a->y * b->z - a->z * b->y;
-    v->y = a->z * b->x - a->x * b->z;
-    v->z = a->x * b->y - a->y * b->x;
+  v->x = a->y * b->z - a->z * b->y;
+  v->y = a->z * b->x - a->x * b->z;
+  v->z = a->x * b->y - a->y * b->x;
+  return v;
+}
+
+CT_Vec3f *ct_normalize3f_imm(CT_Vec3f *v, float len) {
+  float m = v->x * v->x + v->y * v->y + v->z * v->z;
+  if (m > 0.0) {
+    len /= m;
+    v->x *= len;
+    v->y *= len;
+    v->z *= len;
   }
   return v;
+}
+
+CT_Vec3f *ct_normalize3f(CT_Vec3f *v, float len, CT_MPool *mpool) {
+  return ct_normalize3f_imm(ct_set3fv(ALLOCATE_TYPE(mpool, CT_Vec3f), v), len);
 }
--- vec.h
+++ vec.h
@@ -10,17 +10,57 @@ typedef struct {
   float x,y,z;
 } CT_Vec3f;
 
+#define DECL_VEC2OP(type, type2, name)                          \
+  type *name##v(type *a, type *b, CT_MPool* mpool);             \
+  type *name##v_imm(type *a, type *b);                          \
+  type *name##n(type *v, type2 n, CT_MPool* mpool);             \
+  type *name##n_imm(type *v, type2 n);                          \
+  type *name##xy(type *v, type2 x, type2 y, CT_MPool* mpool);   \
+  type *name##xy_imm(type *v, type2 x, type2 y);
+
+#define VEC2OP(type, type2, name, set, op)                              \
+  type *name##v_imm(type *a, type *b) {                                 \
+    a->x op##= b->x;                                                    \
+    a->y op##= b->y;                                                    \
+    return a;                                                           \
+  }                                                                     \
+                                                                        \
+  type *name##v(type *a, type *b, CT_MPool* mpool) {                    \
+    return name##v_imm(set##v(ALLOCATE_TYPE(mpool, type), a), b);       \
+  }                                                                     \
+                                                                        \
+  type *name##n_imm(type *v, type2 n) {                                 \
+    v->x op##= n;                                                       \
+    v->y op##= n;                                                       \
+    return v;                                                           \
+  }                                                                     \
+                                                                        \
+  type *name##n(type *a, type2 n, CT_MPool* mpool) {                    \
+    return name##n_imm(set##v(ALLOCATE_TYPE(mpool, type), a), n);       \
+  }                                                                     \
+                                                                        \
+  type *name##xy_imm(type *v, type2 x, type2 y) {                       \
+    v->x op##= x;                                                       \
+    v->y op##= y;                                                       \
+    return v;                                                           \
+  }                                                                     \
+                                                                        \
+  type *name##xy(type *a, type2 x, type2 y, CT_MPool* mpool) {          \
+    return name##xy_imm(set##v(ALLOCATE_TYPE(mpool, type), a), x, y);   \
+  }
+
+
 CT_Vec2f *ct_vec2f(float x, float y, CT_MPool* mpool);
 CT_Vec2f *ct_set2fv(CT_Vec2f *a, CT_Vec2f *b);
 CT_Vec2f *ct_set2fxy(CT_Vec2f *v, float x, float y);
-CT_Vec2f *ct_add2fv(CT_Vec2f *a, CT_Vec2f *b, CT_MPool* mpool);
-CT_Vec2f *ct_add2fv_imm(CT_Vec2f *a, CT_Vec2f *b);
-CT_Vec2f *ct_add2fn(CT_Vec2f *v, float n, CT_MPool* mpool);
-CT_Vec2f *ct_add2fn_imm(CT_Vec2f *v, float n);
-CT_Vec2f *ct_add2fxy(CT_Vec2f *v, float x, float y, CT_MPool* mpool);
-CT_Vec2f *ct_add2fxy_imm(CT_Vec2f *v, float x, float y);
+DECL_VEC2OP(CT_Vec2f, float, ct_add2f)
+DECL_VEC2OP(CT_Vec2f, float, ct_sub2f)
+DECL_VEC2OP(CT_Vec2f, float, ct_mul2f)
+DECL_VEC2OP(CT_Vec2f, float, ct_div2f)
 
 float ct_dot2fv(CT_Vec2f *a, CT_Vec2f *b);
+CT_Vec2f *ct_normalize2f_imm(CT_Vec2f *v, float len);
+CT_Vec2f *ct_normalize2f(CT_Vec2f *v, float len, CT_MPool *mpool);
 
 CT_Vec3f *ct_vec3f(float x, float y, float z, CT_MPool* mpool);
 CT_Vec3f *ct_set3fv(CT_Vec3f *a, CT_Vec3f *b);
@@ -29,8 +69,10 @@ CT_Vec3f *ct_add3fv(CT_Vec3f *a, CT_Vec3f *b, CT_MPool* mpool);
 CT_Vec3f *ct_add3fv_imm(CT_Vec3f *a, CT_Vec3f *b);
 CT_Vec3f *ct_add3fn(CT_Vec3f *a, float n, CT_MPool* mpool);
 CT_Vec3f *ct_add3fn_imm(CT_Vec3f *a, float n);
-CT_Vec3f *ct_add3fxyz(CT_Vec2f *v, float x, float y, float z, CT_MPool* mpool);
-CT_Vec3f *ct_add3fxyz_imm(CT_Vec2f *v, float x, float y, float z);
+CT_Vec3f *ct_add3fxyz(CT_Vec3f *v, float x, float y, float z, CT_MPool* mpool);
+CT_Vec3f *ct_add3fxyz_imm(CT_Vec3f *v, float x, float y, float z);
 
 float ct_dot3fv(CT_Vec3f *a, CT_Vec3f *b);
 CT_Vec3f *ct_cross3fv(CT_Vec3f *a, CT_Vec3f *b, CT_MPool* mpool);
+CT_Vec3f *ct_normalize3f_imm(CT_Vec3f *v, float len);
+CT_Vec3f *ct_normalize3f(CT_Vec3f *v, float len, CT_MPool *mpool);
