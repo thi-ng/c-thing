4b29967d73f34d8d014f5ec74d29bff406b619b0
Karsten Schmidt <k@postspectacular.com>
2016-05-09 11:34:57 +0100

    add ct_math.h

---
+++ ct_math.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <math.h>
+
+#define EPS 1e-6f
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+static inline uint8_t ct_deltaeqf(float a, float b, float eps) {
+    return fabs(a - b) <= eps;
+}
