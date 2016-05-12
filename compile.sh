217b5eef0bb2a215a499da29ed399a8b921b364f
Karsten Schmidt <k@postspectacular.com>
2016-05-12 11:17:39 +0100

    update compile.sh, reduce stack size to 1MB

--- compile.sh
+++ compile.sh
@@ -46,12 +46,16 @@ echo "using flags: $FLAGS"
 emcc -Os -Isrc \
      $FLAGS \
      --memory-init-file 0 \
-     -s ASM_JS=1 -s INVOKE_RUN=0 \
+     -s ASM_JS=1 \
+     -s INVOKE_RUN=0 \
      -s AGGRESSIVE_VARIABLE_ELIMINATION=1 \
+     -s "TOTAL_STACK=1*1024*1024" \
      -s "EXPORT_NAME='geom'" \
      -s MODULARIZE=1 \
      -o geom.js \
      src/mpool.c all.c \
      $TESTS
+
+cp geom.js geom-cljs/resources/public/js/
 
 # -s EXPORTED_FUNCTIONS=@exports.json
