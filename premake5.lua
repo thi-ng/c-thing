d01c0c0564aefc086dd73905200f4075f10a6122
Karsten Schmidt <k@postspectacular.com>
2016-05-14 00:21:19 +0100

    update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,21 @@
 workspace "cthing"
 configurations { "debug", "release", "test" }
+language "C"
+includedirs { "src" }
+targetdir "bin/%{cfg.buildcfg}"
+files { "src/**.h", "src/**.c" }
+flags { "Symbols", "FatalWarnings", "C++11" }
 
 ----- test
 
 project "test"
 removeconfigurations { "debug", "release" }
+removeflags "Symbols"
 kind "ConsoleApp"
-language "C"
-targetdir "bin/test"
-includedirs { "src" }
-files { "src/**.h", "src/**.c", "test/**.c" }
+files { "test/**.c" }
 
 filter "configurations:test"
 defines { "DEBUG", "TRACE_MPOOL" }
-flags { "Symbols" }
 optimize "Size"
 
 ----- lib
@@ -21,14 +23,11 @@ optimize "Size"
 project "lib"
 removeconfigurations "test"
 kind "StaticLib"
-language "C"
-targetdir "bin/%{cfg.buildcfg}"
-files { "src/**.h", "src/**.c" }
+targetname "thing-geom"
 
 filter "configurations:debug"
 defines { "DEBUG" }
-flags { "Symbols" }
 
 filter "configurations:release"
 defines { "NDEBUG" }
-optimize "On"
+optimize "Size"
