e1bd81b6bf2ff7ab5e6e18a179730e34714703a8
Karsten Schmidt <k@postspectacular.com>
2016-05-14 02:21:32 +0100

    update triangle

--- src/triangle.c
+++ src/triangle.c
@@ -1,39 +1,29 @@
 #include "triangle.h"
 
-CT_EXPORT CT_Triangle2f *ct_triangle2fp(float *points, CT_MPool *mpool) {
-  CT_Triangle2f *t = ALLOCATE_TYPE(mpool, CT_Triangle2f);
-  if (t != NULL) {
-    t->buf[0] = *points++;
-    t->buf[1] = *points++;
-    t->buf[2] = *points++;
-    t->buf[3] = *points++;
-    t->buf[4] = *points++;
-    t->buf[5] = *points;
-  }
+CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, CT_Vec2f *a,
+                                            CT_Vec2f *b, CT_Vec2f *c) {
+  t->a = *a;
+  t->b = *b;
+  t->c = *c;
   return t;
 }
 
-CT_EXPORT CT_Triangle2f *ct_triangle2fv(CT_Vec2f *a, CT_Vec2f *b, CT_Vec2f *c,
-                                        CT_MPool *mpool) {
-  CT_Triangle2f *t = ALLOCATE_TYPE(mpool, CT_Triangle2f);
-  if (t != NULL) {
-    t->a = *a;
-    t->b = *b;
-    t->c = *c;
-  }
+CT_EXPORT CT_Triangle2f *ct_triangle2f_initp(CT_Triangle2f *t,
+                                             CT_Vec2f *points) {
+  t->a = *points++;
+  t->b = *points++;
+  t->c = *points;
   return t;
 }
 
-CT_EXPORT CT_Triangle2f *ct_triangle2fxy(float x1, float y1, float x2, float y2,
-                                         float x3, float y3, CT_MPool *mpool) {
-  CT_Triangle2f *t = ALLOCATE_TYPE(mpool, CT_Triangle2f);
-  if (t != NULL) {
-    t->buf[0] = x1;
-    t->buf[1] = y1;
-    t->buf[2] = x2;
-    t->buf[3] = y2;
-    t->buf[4] = x3;
-    t->buf[5] = y3;
-  }
+CT_EXPORT CT_Triangle2f *ct_triangle2f_initxy(CT_Triangle2f *t, float x1,
+                                              float y1, float x2, float y2,
+                                              float x3, float y3) {
+  t->buf[0] = x1;
+  t->buf[1] = y1;
+  t->buf[2] = x2;
+  t->buf[3] = y2;
+  t->buf[4] = x3;
+  t->buf[5] = y3;
   return t;
 }
--- src/triangle.h
+++ src/triangle.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "mpool.h"
 #include "vec.h"
 
 typedef union {
@@ -9,3 +8,11 @@ typedef union {
   };
   float buf[6];
 } CT_Triangle2f;
+
+CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, CT_Vec2f *a,
+                                            CT_Vec2f *b, CT_Vec2f *c);
+CT_EXPORT CT_Triangle2f *ct_triangle2f_initp(CT_Triangle2f *t,
+                                             CT_Vec2f *points);
+CT_EXPORT CT_Triangle2f *ct_triangle2f_initxy(CT_Triangle2f *t, float x1,
+                                              float y1, float x2, float y2,
+                                              float x3, float y3);
