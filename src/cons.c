9481d224b0e16dd83e79454e1f99f9f94ce9d97b
Karsten Schmidt <k@postspectacular.com>
2016-05-17 22:53:35 +0100

    update/bugfix cons: retain list order when cloning, add iterate_n & take fns

--- src/cons.c
+++ src/cons.c
@@ -5,6 +5,7 @@
 
 typedef struct {
   CT_Cons* head;
+  CT_Cons* prev;
   CT_MPool* pool;
 } CT_ConsCloneState;
 
@@ -17,23 +18,33 @@ fail:
   return cell;
 }
 
-static void ct_cons_clone(CT_Cons* c, void* cstate) {
+static void ct_cons_iterate_clone(CT_Cons* c, void* cstate) {
   CT_ConsCloneState* state = (CT_ConsCloneState*)cstate;
-  //CT_DEBUG("clone: %p, %s, head: %p", c->value, c->value, state->head);
-  state->head = ct_cons(c->value, state->head, state->pool);
-  //CT_DEBUG("car: %p, val: %p, %s, cdr: %p", state->head, state->head->value, state->head->value, state->head->next);
+  c = ct_cons(c->value, NULL, state->pool);
+  if (state->head == NULL) {
+    state->head = c;
+    state->prev = c;
+  } else {
+    state->prev->next = c;
+    state->prev = c;
+  }
 }
 
 CT_EXPORT void ct_cons_free(CT_Cons* cell, size_t freeVal) {
-  CT_Cons* next = NULL;
-  while (cell != NULL) {
-    next = cell->next;
+  if (cell != NULL) {
     if (freeVal) {
       CT_INFO("free cons value: %p", cell->value);
       free(cell->value);
     }
-    CT_INFO("free cons: %p", cell);
     free(cell);
+  }
+}
+
+CT_EXPORT void ct_cons_free_all(CT_Cons* cell, size_t freeVal) {
+  CT_Cons* next = NULL;
+  while (cell != NULL) {
+    next = cell->next;
+    ct_cons_free(cell, freeVal);
     cell = next;
   }
 }
@@ -41,6 +52,15 @@ CT_EXPORT void ct_cons_free(CT_Cons* cell, size_t freeVal) {
 CT_EXPORT void* ct_cons_iterate(CT_Cons* cell, CT_ConsVisitor visit,
                                 void* state) {
   while (cell != NULL) {
+    visit(cell, state);
+    cell = cell->next;
+  }
+  return state;
+}
+
+CT_EXPORT void* ct_cons_iterate_n(CT_Cons* cell, CT_ConsVisitor visit,
+                                  void* state, size_t num) {
+  while (cell != NULL && num--) {
     visit(cell, state);
     cell = cell->next;
   }
@@ -71,10 +91,12 @@ CT_EXPORT CT_Cons* ct_cons_from_parray(void** values, size_t num, CT_Cons* head,
 }
 
 CT_EXPORT CT_Cons* ct_cons_concat_imm(CT_Cons* head, CT_Cons* rest) {
-  CT_Cons* hd = head;
-  if (hd != NULL) {
+  if (head != NULL) {
+    CT_Cons* hd = head;
     while (1) {
+      //CT_INFO("search: %p, val: %s, next: %p", hd, (char*)hd->value, hd->next);
       if (hd->next == NULL) {
+        //CT_INFO("link: %p, %p", hd, rest);
         hd->next = rest;
         break;
       }
@@ -87,7 +109,13 @@ CT_EXPORT CT_Cons* ct_cons_concat_imm(CT_Cons* head, CT_Cons* rest) {
 CT_EXPORT CT_Cons* ct_cons_concat(CT_Cons* head, CT_Cons* rest,
                                   CT_MPool* mpool) {
   CT_ConsCloneState state = {.head = NULL, .pool = mpool};
-  ct_cons_iterate(head, ct_cons_clone,
-                  ct_cons_iterate(rest, ct_cons_clone, &state));
+  ct_cons_iterate(head, ct_cons_iterate_clone,
+                  ct_cons_iterate(rest, ct_cons_iterate_clone, &state));
+  return state.head;
+}
+
+CT_EXPORT CT_Cons* ct_cons_take(CT_Cons* head, size_t num, CT_MPool* mpool) {
+  CT_ConsCloneState state = {.head = NULL, .pool = mpool};
+  ct_cons_iterate_n(head, ct_cons_iterate_clone, &state, num);
   return state.head;
 }
