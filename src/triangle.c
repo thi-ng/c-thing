bdc57ff22b493c3071b1cdfb84acbb3051f7e0c1
Karsten Schmidt <k@postspectacular.com>
2016-05-31 22:55:03 +0100

    add triangle_point_opposite fns, add const qualifiers

--- src/triangle.c
+++ src/triangle.c
@@ -1,7 +1,8 @@
 #include "triangle.h"
 
-CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, CT_Vec2f *a,
-                                            CT_Vec2f *b, CT_Vec2f *c) {
+CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, const CT_Vec2f *a,
+                                            const CT_Vec2f *b,
+                                            const CT_Vec2f *c) {
   t->a = *a;
   t->b = *b;
   t->c = *c;
@@ -9,14 +10,15 @@ CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, CT_Vec2f *a,
 }
 
 CT_EXPORT CT_Triangle2f *ct_triangle2f_initpv(CT_Triangle2f *t,
-                                              CT_Vec2f *points) {
+                                              const CT_Vec2f *points) {
   t->a = *points++;
   t->b = *points++;
   t->c = *points;
   return t;
 }
 
-CT_EXPORT CT_Triangle2f *ct_triangle2f_initpf(CT_Triangle2f *t, float *coords) {
+CT_EXPORT CT_Triangle2f *ct_triangle2f_initpf(CT_Triangle2f *t,
+                                              const float *coords) {
   t->buf[0] = *coords++;
   t->buf[1] = *coords++;
   t->buf[2] = *coords++;
@@ -26,26 +28,59 @@ CT_EXPORT CT_Triangle2f *ct_triangle2f_initpf(CT_Triangle2f *t, float *coords) {
   return t;
 }
 
-float ct_triangle2f_normsign(CT_Triangle2f *t) {
+CT_EXPORT float ct_triangle2f_normsign(const CT_Triangle2f *t) {
   return (t->b.x - t->a.x) * (t->c.y - t->a.y) -
          (t->c.x - t->a.x) * (t->b.y - t->a.y);
 }
 
-CT_EXPORT float ct_triangle2f_area(CT_Triangle2f *t) {
+CT_EXPORT float ct_triangle2f_area(const CT_Triangle2f *t) {
   return 0.5f * ct_triangle2f_normsign(t);
 }
 
-CT_EXPORT float ct_triangle2f_circumference(CT_Triangle2f *t) {
+CT_EXPORT float ct_triangle2f_circumference(const CT_Triangle2f *t) {
   return ct_dist2fv(&t->a, &t->b) + ct_dist2fv(&t->b, &t->c) +
          ct_dist2fv(&t->c, &t->a);
 }
 
-CT_EXPORT size_t ct_triangle2f_is_clockwise(CT_Triangle2f *t) {
+CT_EXPORT size_t ct_triangle2f_is_clockwise(const CT_Triangle2f *t) {
   return ct_triangle2f_normsign(t) < 0.f;
 }
 
-CT_EXPORT CT_Triangle3f *ct_triangle3f_init(CT_Triangle3f *t, CT_Vec3f *a,
-                                            CT_Vec3f *b, CT_Vec3f *c) {
+CT_EXPORT CT_Vec2f *ct_triangle2f_point_opposite_ptr(CT_Triangle2f *t,
+                                                     const CT_Vec2f *a,
+                                                     const CT_Vec2f *b) {
+  if (a == &t->a) {
+    return (b == &t->b ? &t->c : (b == &t->c ? &t->b : NULL));
+  } else if (a == &t->b) {
+    return (b == &t->a ? &t->c : (b == &t->c ? &t->a : NULL));
+  } else if (a == &t->c) {
+    return (b == &t->b ? &t->a : (b == &t->a ? &t->b : NULL));
+  }
+  return NULL;
+}
+
+CT_EXPORT CT_Vec2f *ct_triangle2f_point_opposite(CT_Triangle2f *t,
+                                                 const CT_Vec2f *a,
+                                                 const CT_Vec2f *b) {
+  if (ct_deltaeq2fv(a, &t->a, EPS)) {
+    return (ct_deltaeq2fv(b, &t->b, EPS)
+                ? &t->c
+                : (ct_deltaeq2fv(b, &t->c, EPS) ? &t->b : NULL));
+  } else if (ct_deltaeq2fv(a, &t->b, EPS)) {
+    return (ct_deltaeq2fv(b, &t->a, EPS)
+                ? &t->c
+                : (ct_deltaeq2fv(b, &t->c, EPS) ? &t->a : NULL));
+  } else if (ct_deltaeq2fv(a, &t->c, EPS)) {
+    return (ct_deltaeq2fv(b, &t->b, EPS)
+                ? &t->a
+                : (ct_deltaeq2fv(b, &t->a, EPS) ? &t->b : NULL));
+  }
+  return NULL;
+}
+
+CT_EXPORT CT_Triangle3f *ct_triangle3f_init(CT_Triangle3f *t, const CT_Vec3f *a,
+                                            const CT_Vec3f *b,
+                                            const CT_Vec3f *c) {
   t->a = *a;
   t->b = *b;
   t->c = *c;
@@ -53,14 +88,15 @@ CT_EXPORT CT_Triangle3f *ct_triangle3f_init(CT_Triangle3f *t, CT_Vec3f *a,
 }
 
 CT_EXPORT CT_Triangle3f *ct_triangle3f_initpv(CT_Triangle3f *t,
-                                              CT_Vec3f *points) {
+                                              const CT_Vec3f *points) {
   t->a = *points++;
   t->b = *points++;
   t->c = *points;
   return t;
 }
 
-CT_EXPORT CT_Triangle3f *ct_triangle3f_initpf(CT_Triangle3f *t, float *coords) {
+CT_EXPORT CT_Triangle3f *ct_triangle3f_initpf(CT_Triangle3f *t,
+                                              const float *coords) {
   t->a.x = *coords++;
   t->a.y = *coords++;
   t->a.z = *coords++;
@@ -73,7 +109,7 @@ CT_EXPORT CT_Triangle3f *ct_triangle3f_initpf(CT_Triangle3f *t, float *coords) {
   return t;
 }
 
-CT_EXPORT CT_Vec3f *ct_triangle3f_normal(CT_Triangle3f *t, CT_Vec3f *out,
+CT_EXPORT CT_Vec3f *ct_triangle3f_normal(const CT_Triangle3f *t, CT_Vec3f *out,
                                          size_t normalize) {
   CT_Vec3f ba, ca;
   ct_cross3fv(ct_sub3fv(&t->b, &t->a, &ba), ct_sub3fv(&t->c, &t->a, &ca), out);
@@ -83,7 +119,7 @@ CT_EXPORT CT_Vec3f *ct_triangle3f_normal(CT_Triangle3f *t, CT_Vec3f *out,
   return out;
 }
 
-float ct_triangle3f_normsign(CT_Triangle3f *t) {
+CT_EXPORT float ct_triangle3f_normsign(const CT_Triangle3f *t) {
   CT_Vec3f n;
   return ct_mag3f(ct_triangle3f_normal(t, &n, 0));
 }
@@ -97,7 +133,40 @@ CT_EXPORT float ct_triangle3f_circumference(CT_Triangle3f *t) {
          ct_dist3fv(&t->c, &t->a);
 }
 
-CT_EXPORT size_t ct_triangle3f_is_clockwise(CT_Triangle3f *t, CT_Vec3f *n) {
+CT_EXPORT size_t ct_triangle3f_is_clockwise(const CT_Triangle3f *t,
+                                            const CT_Vec3f *n) {
   CT_Vec3f tn;
   return ct_dot3fv(ct_triangle3f_normal(t, &tn, 0), n) > 0.f;
+}
+
+CT_EXPORT CT_Vec3f *ct_triangle3f_point_opposite_ptr(CT_Triangle3f *t,
+                                                     const CT_Vec3f *a,
+                                                     const CT_Vec3f *b) {
+  if (a == &t->a) {
+    return (b == &t->b ? &t->c : (b == &t->c ? &t->b : NULL));
+  } else if (a == &t->b) {
+    return (b == &t->a ? &t->c : (b == &t->c ? &t->a : NULL));
+  } else if (a == &t->c) {
+    return (b == &t->b ? &t->a : (b == &t->a ? &t->b : NULL));
+  }
+  return NULL;
+}
+
+CT_EXPORT CT_Vec3f *ct_triangle3f_point_opposite(CT_Triangle3f *t,
+                                                 const CT_Vec3f *a,
+                                                 const CT_Vec3f *b) {
+  if (ct_deltaeq3fv(a, &t->a, EPS)) {
+    return (ct_deltaeq3fv(b, &t->b, EPS)
+                ? &t->c
+                : (ct_deltaeq3fv(b, &t->c, EPS) ? &t->b : NULL));
+  } else if (ct_deltaeq3fv(a, &t->b, EPS)) {
+    return (ct_deltaeq3fv(b, &t->a, EPS)
+                ? &t->c
+                : (ct_deltaeq3fv(b, &t->c, EPS) ? &t->a : NULL));
+  } else if (ct_deltaeq3fv(a, &t->c, EPS)) {
+    return (ct_deltaeq3fv(b, &t->b, EPS)
+                ? &t->a
+                : (ct_deltaeq3fv(b, &t->a, EPS) ? &t->b : NULL));
+  }
+  return NULL;
 }
--- src/triangle.h
+++ src/triangle.h
@@ -19,22 +19,36 @@ typedef union {
   float buf[3 * sizeof(CT_Vec3f) / sizeof(float)];
 } CT_Triangle3f;
 
-CT_EXPORT CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, CT_Vec2f *a,
-                                            CT_Vec2f *b, CT_Vec2f *c);
-CT_EXPORT CT_Triangle2f *ct_triangle2f_initpv(CT_Triangle2f *t,
-                                              CT_Vec2f *points);
-CT_EXPORT CT_Triangle2f *ct_triangle2f_initpf(CT_Triangle2f *t, float *coords);
+CT_Triangle2f *ct_triangle2f_init(CT_Triangle2f *t, const CT_Vec2f *a,
+                                  const CT_Vec2f *b, const CT_Vec2f *c);
+CT_Triangle2f *ct_triangle2f_initpv(CT_Triangle2f *t, const CT_Vec2f *points);
+CT_Triangle2f *ct_triangle2f_initpf(CT_Triangle2f *t, const float *coords);
 
-CT_EXPORT float ct_triangle2f_area(CT_Triangle2f *t);
-CT_EXPORT float ct_triangle2f_circumference(CT_Triangle2f *t);
+float ct_triangle2f_area(const CT_Triangle2f *t);
+float ct_triangle2f_circumference(const CT_Triangle2f *t);
+float ct_triangle2f_normsign(const CT_Triangle2f *t);
+size_t ct_triangle2f_is_clockwise(const CT_Triangle2f *t);
+CT_Vec2f *ct_triangle2f_point_opposite_ptr(CT_Triangle2f *t, const CT_Vec2f *a,
+                                           const CT_Vec2f *b);
+CT_Vec2f *ct_triangle2f_point_opposite(CT_Triangle2f *t, const CT_Vec2f *a,
+                                       const CT_Vec2f *b);
 
-CT_EXPORT CT_Triangle3f *ct_triangle3f_init(CT_Triangle3f *t, CT_Vec3f *a,
-                                            CT_Vec3f *b, CT_Vec3f *c);
-CT_EXPORT CT_Triangle3f *ct_triangle3f_initpv(CT_Triangle3f *t,
-                                              CT_Vec3f *points);
-CT_EXPORT CT_Triangle3f *ct_triangle3f_initpf(CT_Triangle3f *t, float *coords);
+///// CT_Triangle3f
 
-CT_EXPORT float ct_triangle3f_area(CT_Triangle3f *t);
-CT_EXPORT float ct_triangle3f_circumference(CT_Triangle3f *t);
+CT_Triangle3f *ct_triangle3f_init(CT_Triangle3f *t, const CT_Vec3f *a,
+                                  const CT_Vec3f *b, const CT_Vec3f *c);
+CT_Triangle3f *ct_triangle3f_initpv(CT_Triangle3f *t, const CT_Vec3f *points);
+CT_Triangle3f *ct_triangle3f_initpf(CT_Triangle3f *t, const float *coords);
+
+float ct_triangle3f_area(CT_Triangle3f *t);
+float ct_triangle3f_circumference(CT_Triangle3f *t);
+CT_Vec3f *ct_triangle3f_normal(const CT_Triangle3f *t, CT_Vec3f *out,
+                               size_t normalize);
+float ct_triangle3f_normsign(const CT_Triangle3f *t);
+size_t ct_triangle3f_is_clockwise(const CT_Triangle3f *t, const CT_Vec3f *n);
+CT_Vec3f *ct_triangle3f_point_opposite_ptr(CT_Triangle3f *t, const CT_Vec3f *a,
+                                           const CT_Vec3f *b);
+CT_Vec3f *ct_triangle3f_point_opposite(CT_Triangle3f *t, const CT_Vec3f *a,
+                                       const CT_Vec3f *b);
 
 CT_END_DECLS
