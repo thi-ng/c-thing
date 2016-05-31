83effca460aa9728385481adf0ccb43662c51464
Karsten Schmidt <k@postspectacular.com>
2016-05-31 17:40:59 +0100

    add isec.c/h

---
+++ src/isec.c
@@ -0,0 +1,10 @@
+#include "isec.h"
+
+CT_EXPORT int ct_intersect_rect_circle(const CT_Vec2f *p, const CT_Vec2f *q,
+                                       const CT_Vec2f *c, float r) {
+  const float dx =
+      (c->x < p->x ? (c->x - p->x) : (c->x > q->x ? (c->x - q->x) : 0.0f));
+  const float dy =
+      (c->y < p->y ? (c->y - p->y) : (c->y > q->y ? (c->y - q->y) : 0.0f));
+  return (dx * dx + dy * dy) <= (r * r);
+}
---
+++ src/isec.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "vec.h"
+
+int ct_intersect_rect_circle(const CT_Vec2f *p, const CT_Vec2f *q,
+                             const CT_Vec2f *c, float r);
