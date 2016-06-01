4019eab4c13190eeca01f2a6e86d9c9abd25fd2b
Karsten Schmidt <k@postspectacular.com>
2016-06-01 14:53:37 +0100

    split hash fns for better reuse

--- src/hashfn.c
+++ src/hashfn.c
@@ -8,6 +8,24 @@
 #define MURMUR_H2 ((uint32_t)0x85ebca6b)
 #define MURMUR_H3 ((uint32_t)0xc2b2ae35)
 
+ct_inline uint32_t mixK(uint32_t k) {
+  return ct_rotl32(k * MURMUR_C1, 15) * MURMUR_C2;
+}
+
+ct_inline uint32_t mixH(uint32_t h, uint32_t k) {
+  return ct_rotl32(h ^ k, 13) * 5 + MURMUR_H1;
+}
+
+ct_inline uint32_t fmix(uint32_t h, size_t len) {
+  h ^= len;
+  h ^= h >> 16;
+  h *= MURMUR_H2;
+  h ^= h >> 13;
+  h *= MURMUR_H3;
+  h ^= h >> 16;
+  return h;
+}
+
 CT_EXPORT uint32_t ct_murmur3_32(const void *data, size_t numBytes) {
   if (data == NULL || numBytes == 0) return 0;
 
@@ -16,13 +34,11 @@ CT_EXPORT uint32_t ct_murmur3_32(const void *data, size_t numBytes) {
   const uint8_t *tail = (const uint8_t *)&data[numBlocks << 2];
 
   uint32_t h = 0;
-  uint32_t k;
+  uint32_t k = 0;
   for (size_t i = 0; i < numBlocks; i++) {
-    k = ct_rotl32(blocks[i] * MURMUR_C1, 15) * MURMUR_C2;
-    h = ct_rotl32(h ^ k, 13) * 5 + MURMUR_H1;
+    h = mixH(h, mixK(blocks[i]));
   }
 
-  k = 0;
   switch (numBytes & 3) {
     case 3:
       k ^= tail[2] << 16;
@@ -36,12 +52,5 @@ CT_EXPORT uint32_t ct_murmur3_32(const void *data, size_t numBytes) {
       h ^= k;
   }
 
-  h ^= numBytes;
-  h ^= h >> 16;
-  h *= MURMUR_H2;
-  h ^= h >> 13;
-  h *= MURMUR_H3;
-  h ^= h >> 16;
-
-  return h;
+  return fmix(h, numBytes);
 }
