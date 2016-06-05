622dea30be6c2e9b4db90832312417dda8b16171
Karsten Schmidt <k@postspectacular.com>
2016-06-05 23:51:35 +0100

    add elaine.h image header, update poisson SVG test w/ imagebased sampling

--- test/poisson.c
+++ test/poisson.c
@@ -2,36 +2,52 @@
 
 #include "common/dbg.h"
 #include "common/test.h"
+#include "io/svg.h"
+#include "math/math.h"
 #include "math/poisson.h"
 
+#include "elaine.h"
+
 CT_TEST_DECLS
+
+static uint32_t w = 512;
+static uint32_t h = 512;
+
+static float disc_gen(CT_Quadtree *t, CT_Circle2f *disc) {
+  ct_set2fxy(&disc->pos, ct_rand_normpos() * t->root.w,
+             ct_rand_normpos() * t->root.h);
+  //return disc->r = (sinf(disc->pos.x * 0.04f) * cosf(disc->pos.y * 0.005f + PI)) * 10 + 11.5f;
+  //return disc->r = ct_mixf(4, 50, ct_dist2fv(&t->root.centroid, &disc->pos) / 600.0);
+  size_t x       = (size_t)disc->pos.x;
+  size_t y       = (size_t)disc->pos.y;
+  float tt       = 1.0f - (float)elaine_gray8[y * w + x] / 255.f;
+  return disc->r = ct_mixf(1.5f, 8.f, powf(tt, 3.f));
+}
 
 #ifndef __EMSCRIPTEN__
 int test_poisson_svg() {
   CT_Quadtree t;
   CT_MPool vpool;
-  int w = 600, h = 600;
-  CT_IS(!ct_qtree_init(&t, 0, 0, w, h, 0x10000), "init");
+  CT_IS(!ct_qtree_init(&t, 0, 0, w, h, 0x1000), "init");
   CT_IS(!ct_mpool_init(&vpool, 0x1000, sizeof(CT_Vec2f)), "init pool");
   FILE *f = fopen("poisson.svg", "w");
-  fprintf(f,
-          "<?xml version=\"1.0\"?>\n<svg version=\"1.1\" width=\"%u\" "
-          "height=\"%u\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" "
-          "xmlns=\"http://www.w3.org/2000/svg\">\n",
-          w, h);
-  fprintf(f, "<g stroke=\"none\" fill=\"black\">\n");
+  char buf[0x10000];
+  setvbuf(f, buf, _IOFBF, 0x1000);
+  ct_svg_write_header(
+      f, ct_svg_attribs(1, 2, SVG_INT("width", w), SVG_INT("height", h)));
+  ct_svg_write_rect(f, 0, 0, w, h, ct_svg_attribs(1, 1, SVG_HEX("fill", 0xff)));
+  fprintf(f, "<g stroke=\"none\" fill=\"white\">\n");
   srand(time(0));
-  float r       = 10;
   size_t count  = 0;
   size_t failed = 0;
   CT_Vec2f *p   = (CT_Vec2f *)ct_mpool_alloc(&vpool);
   while (1) {
-    if (!ct_poisson_sample2f(&t, r, 20, p)) {
-      fprintf(f, "<circle cx=\"%f\" cy=\"%f\" r=\"%f\"/>", p->x, p->y, 2.f);
+    if (!ct_poisson_sample2f_with(&t, disc_gen, 5, p)) {
+      ct_svg_write_circle(f, p->x, p->y, 1.f, NULL);
       p = (CT_Vec2f *)ct_mpool_alloc(&vpool);
       count++;
       failed = 0;
-    } else if (++failed > 50) {
+    } else if (++failed > 1e3) {
       break;
     }
   }
