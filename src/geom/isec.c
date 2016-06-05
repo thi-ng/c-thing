80f175f403495c72e5cfe76f2c6adad68aa9e05d
Karsten Schmidt <k@postspectacular.com>
2016-06-05 13:32:52 +0100

    update intersect_rect_circle, add intersect_aabb_sphere

--- src/geom/isec.c
+++ src/geom/isec.c
@@ -1,10 +1,18 @@
 #include "geom/isec.h"
 
+#define DELTA(a, b, c) ((a) < (b) ? ((a) - (b)) : ((a) > (c) ? ((a) - (c)) : 0))
+
 CT_EXPORT int ct_intersect_rect_circle(const CT_Vec2f *p, const CT_Vec2f *q,
                                        const CT_Vec2f *c, float r) {
-  const float dx =
-      (c->x < p->x ? (c->x - p->x) : (c->x > q->x ? (c->x - q->x) : 0.0f));
-  const float dy =
-      (c->y < p->y ? (c->y - p->y) : (c->y > q->y ? (c->y - q->y) : 0.0f));
+  const float dx = DELTA(c->x, p->x, q->x);
+  const float dy = DELTA(c->y, p->y, q->y);
   return (dx * dx + dy * dy) <= (r * r);
+}
+
+CT_EXPORT int ct_intersect_aabb_sphere(const CT_Vec3f *p, const CT_Vec3f *q,
+                                       const CT_Vec3f *c, float r) {
+  const float dx = DELTA(c->x, p->x, q->x);
+  const float dy = DELTA(c->y, p->y, q->y);
+  const float dz = DELTA(c->z, p->z, q->z);
+  return (dx * dx + dy * dy + dz * dz) <= (r * r);
 }
--- src/geom/isec.h
+++ src/geom/isec.h
@@ -2,5 +2,9 @@
 
 #include "math/vec.h"
 
+// p/q = rect min/max, c = circle origin, r = radius
 int ct_intersect_rect_circle(const CT_Vec2f *p, const CT_Vec2f *q,
                              const CT_Vec2f *c, float r);
+
+int ct_intersect_aabb_sphere(const CT_Vec3f *p, const CT_Vec3f *q,
+                             const CT_Vec3f *c, float r);
