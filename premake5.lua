0c86f0da40267336121cc22dbf17b9b75ad62c12
Karsten Schmidt <k@postspectacular.com>
2016-06-06 01:57:41 +0100

    add readme, update premake file

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ language "C"
 includedirs { "src" }
 targetdir "bin/%{cfg.buildcfg}"
 files { "src/**.h", "src/**.c" }
-removefiles {"src/geom/mesh.c", "src/reflist.c" }
+removefiles {"src/geom/mesh.c" }
 flags { "Symbols", "FatalWarnings", "C++11" }
 
 ----- test
@@ -15,19 +15,20 @@ kind "ConsoleApp"
 files { "test/**.c" }
 
 filter "configurations:test"
-defines { "DEBUG", "CT_FEATURE_ANSI", "CT_FEATURE_CHECKS", "CT_FEATURE_SSE" }
+defines { "NDEBUG", "CT_FEATURE_ANSI", "CT_FEATURE_CHECKS", "CT_FEATURE_SSE" }
 optimize "Size"
 
 ----- lib
 
 project "lib"
 removeconfigurations "test"
+removefiles {"src/common/test.c" }
 kind "StaticLib"
-targetname "thing-geom"
+targetname "cthing"
 
 filter "configurations:debug"
 defines { "DEBUG", "CT_FEATURE_CHECKS", "CT_FEATURE_SSE" }
 
 filter "configurations:release"
-defines { "NDEBUG", "CT_FEATURE_CHECKS", "CT_FEATURE_SSE" }
+defines { "NDEBUG", "CT_FEATURE_LOG", "CT_FEATURE_SSE" }
 optimize "Size"
