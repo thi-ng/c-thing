2afa4158f9a2f19ee242974426691de570f83b8f
Karsten Schmidt <k@postspectacular.com>
2016-06-16 20:51:39 +0100

    refactor DLA example

--- examples/dla/main.c
+++ examples/dla/main.c
@@ -7,6 +7,8 @@
 #include "math/math.h"
 #include "math/vec.h"
 
+#define BUF_SIZE 0x400
+
 typedef struct {
   CT_Vec3f *particles;
   CT_SpatialGrid accel;
@@ -15,6 +17,7 @@ typedef struct {
   size_t width;
   size_t height;
   size_t seeds;
+  float eps[2];
 } DLA;
 
 typedef struct {
@@ -24,32 +27,38 @@ typedef struct {
   float radius;
 } DLA_Sel;
 
+// clang-format off
+static DLA dla = {
+  .width  = 600,
+  .height = 600,
+  .seeds  = 16,
+  .eps    = {25, 25}
+};
+// clang-format on
+
 ct_inline CT_Vec3f *rand_particle(DLA *dla) {
-  CT_Vec3f *p = &dla->particles[dla->num++];
-  ct_set3fxyz(p, ct_rand_normpos() * dla->width,
-              ct_rand_normpos() * dla->height, 0);
-  return p;
+  return ct_set3fxyz(&dla->particles[dla->num++],
+                     ct_rand_normpos() * dla->width,
+                     ct_rand_normpos() * dla->height, 0);
 }
 
 static void add_particle(DLA *dla) {
-  CT_Vec3f *res[0x400];
+  CT_Vec3f *res[BUF_SIZE];
   CT_Vec3f *p = rand_particle(dla);
-  float r[2]  = {50, 50};
   while (1) {
-    size_t num =
-        ct_spgrid_select2d(&dla->accel, (float *)p, r, (void **)&res, 0x400);
+    size_t num = ct_spgrid_select2d(&dla->accel, (float *)p, dla->eps,
+                                    (void **)&res, BUF_SIZE);
     if (num > 0) {
       float minD  = 1e9;
       CT_Vec3f *c = NULL;
       for (size_t i = 0; i < num; i++) {
         float d = ct_distsq2fv((CT_Vec2f *)p, (CT_Vec2f *)res[i]);
-        if (d < minD) {
+        if (d >= 1 && d < minD) {
           minD = d;
           c    = res[i];
         }
       }
       if (c) {
-        //CT_INFO("%zd c: %f,%f d: %f", dla->num, c->x, c->y, minD);
         float t = 1.0f / sqrtf(minD);
         p->x    = c->x + t * (p->x - c->x);
         p->y    = c->y + t * (p->y - c->y);
@@ -64,7 +73,6 @@ static void add_particle(DLA *dla) {
 }
 
 int main() {
-  DLA dla = {.width = 600, .height = 600, .seeds = 16};
   dla.length    = dla.width * dla.height >> 2;
   dla.particles = calloc(dla.length, sizeof(CT_Vec3f));
   ct_spgrid_init(&dla.accel, (float[]){0, 0}, (float[]){dla.width, dla.height},
