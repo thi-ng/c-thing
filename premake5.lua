912fc0fa07ad694c4733c89bafdc65c505a0e77e
Karsten Schmidt <k@postspectacular.com>
2016-06-16 20:39:33 +0100

    simplify premake script, update autotest & readme

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 workspace "cthing"
-configurations { "debug", "release", "test" }
+configurations { "debug", "release" }
 platforms { "sse", "no_sse" }
 language "C"
 includedirs { "src" }
@@ -9,66 +9,42 @@ flags { "Symbols", "FatalWarnings", "C++11" }
 filter "platforms:sse"
 defines { "CT_FEATURE_SSE" }
 
------ test
-
-project "test"
-removeconfigurations { "debug", "release" }
-kind "ConsoleApp"
-files { "src/**.h", "src/**.c", "test/**.c" }
-removefiles {"src/geom/mesh.c" }
-
-filter "configurations:test"
-defines { "NDEBUG", "CT_FEATURE_ANSI", "CT_FEATURE_CHECKS", "CT_FEATURE_CHECK_MEM" }
-optimize "Size"
-
------ lib
-
-project "lib"
-removeconfigurations "test"
-files { "src/**.h", "src/**.c" }
-removefiles {"src/geom/mesh.c", "src/common/test.c" }
-kind "StaticLib"
-targetname "cthing"
-
 filter "configurations:debug"
 defines { "DEBUG", "CT_FEATURE_CHECKS", "CT_FEATURE_CHECK_MEM" }
 
 filter "configurations:release"
 defines { "NDEBUG", "CT_FEATURE_LOG" }
 optimize "Size"
+linkoptions { "-flto" }
 
------ examples -----
+----- test
+
+project "test"
+kind "ConsoleApp"
+files { "src/**.h", "src/**.c", "test/**.c" }
+removefiles {"src/geom/mesh.c" }
+defines { "CT_FEATURE_ANSI" }
+
+----- lib
+
+project "lib"
+files { "src/**.h", "src/**.c" }
+removefiles {"src/geom/mesh.c", "src/common/test.c" }
+kind "StaticLib"
+targetname "cthing"
 
 ----- poisson -----
 
 project "ex-poisson"
-removeconfigurations "test"
 kind "ConsoleApp"
 files { "examples/poisson/*.c" }
 includedirs { "examples/common", "examples/poisson" }
 links "lib"
 
-filter "configurations:debug"
-defines { "DEBUG", "CT_FEATURE_ANSI", "CT_FEATURE_CHECKS", "CT_FEATURE_CHECK_MEM" }
-
-filter "configurations:release"
-defines { "NDEBUG", "CT_FEATURE_LOG" }
-optimize "Size"
-linkoptions { "-flto" }
-
 ----- dla -----
 
 project "ex-dla"
-removeconfigurations "test"
 kind "ConsoleApp"
 files { "examples/dla/*.c" }
 includedirs { "examples/common", "examples/dla" }
 links "lib"
-
-filter "configurations:debug"
-defines { "DEBUG", "CT_FEATURE_ANSI", "CT_FEATURE_CHECKS", "CT_FEATURE_CHECK_MEM" }
-
-filter "configurations:release"
-defines { "NDEBUG", "CT_FEATURE_LOG" }
-optimize "Size"
-linkoptions { "-flto" }
