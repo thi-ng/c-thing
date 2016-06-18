6ccf6611043239dc7f8f0db71f2d5f3de607a60f
Karsten Schmidt <k@postspectacular.com>
2016-06-18 15:53:00 +0100

    update rect.c

--- src/geom/rect.h
+++ src/geom/rect.h
@@ -17,7 +17,7 @@ typedef union {
 CT_Rect2f *ct_rect2fv(CT_Vec2f *p, CT_Vec2f *size, CT_MPool *mpool);
 CT_Rect2f *ct_rect2fn(CT_Vec2f *p, float size, CT_MPool *mpool);
 
-float ct_rect2f_area(void *a);
+float ct_rect2f_area(CT_Rect2f *a);
 int ct_rect2f_classify_point(CT_Rect2f *a, CT_Vec2f *p);
 
 CT_END_DECLS
