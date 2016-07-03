54fac537c6a3f6e02ae5881a001b8fb664ee05e2
Karsten Schmidt <k@postspectacular.com>
2016-07-03 17:36:32 +0100

    bugfix postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -2,5 +2,5 @@ fs=require('fs');
 src=fs.readFileSync('cthing.js','utf8');
 src=src.replace(/_ct_\w+_\d+:[\w\$]+,?/g, '');
 src=src.replace(/\w+\._ct_\w+_\d+\=\w+._ct_\w+_\d+;/g, '');
-src=src.replace(/\s{2,}/g,' ');
+src=src.replace(/;\s{2,}/g,'\n');
 fs.writeFileSync('cthing.min.js', src, 'utf8');
