bed1c65ee0175c17e1248bcec6e2f036ea3bdb25
Karsten Schmidt <k@postspectacular.com>
2016-07-16 12:56:31 +0100

    add ct_rect2f_sample_inside

--- src/geom/rect.c
+++ src/geom/rect.c
@@ -1,14 +1,20 @@
 #include "geom/rect.h"
 
-ct_export float ct_rect2f_area(CT_Rect2f *r) {
+ct_export float ct_rect2f_area(const CT_Rect2f *r) {
   return (r->q.x - r->p.x) * (r->q.y - r->p.y);
 }
 
-ct_export int ct_rect2f_classify_point(CT_Rect2f *r, CT_Vec2f *p) {
+ct_export int ct_rect2f_classify_point(const CT_Rect2f *r, const CT_Vec2f *p) {
   if (p->x > r->p.x && p->x < r->q.x && p->y > r->p.y && p->y < r->q.y) {
     return 1;
   } else if (p->x < r->p.x || p->x > r->q.x || p->y < r->p.y || p->y > r->q.y) {
     return -1;
   }
   return 0;  // FIXME
+}
+
+ct_export CT_Vec2f *ct_rect2f_sample_inside(const CT_Rect2f *r, CT_Vec2f *out) {
+  out->x = ct_rand_normpos() * (r->q.x - r->p.x) + r->p.x;
+  out->y = ct_rand_normpos() * (r->q.y - r->p.y) + r->p.y;
+  return out;
 }
--- src/geom/rect.h
+++ src/geom/rect.h
@@ -17,7 +17,8 @@ typedef union {
 CT_Rect2f *ct_rect2fv(CT_Vec2f *p, CT_Vec2f *size, CT_MPool *mpool);
 CT_Rect2f *ct_rect2fn(CT_Vec2f *p, float size, CT_MPool *mpool);
 
-float ct_rect2f_area(CT_Rect2f *a);
-int ct_rect2f_classify_point(CT_Rect2f *a, CT_Vec2f *p);
+float ct_rect2f_area(const CT_Rect2f *a);
+int ct_rect2f_classify_point(const CT_Rect2f *a, const CT_Vec2f *p);
+CT_Vec2f *ct_rect2f_sample_inside(const CT_Rect2f *r, CT_Vec2f *out);
 
 CT_END_DECLS
