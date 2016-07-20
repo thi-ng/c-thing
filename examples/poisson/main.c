ff2e5086ab7df3b04984cc071a8ca05b3c1dbbb4
Karsten Schmidt <k@postspectacular.com>
2016-07-20 14:05:37 +0100

    update poisson example to support raw text output of points

--- examples/poisson/main.c
+++ examples/poisson/main.c
@@ -14,6 +14,8 @@
 #include "stb.h"
 
 typedef struct {
+  CT_Quadtree tree;
+  CT_MPool vpool;
   char *path;
   float minDist;
   float maxDist;
@@ -27,10 +29,11 @@ typedef struct {
   int width;
   int height;
   int channels;
-} Config;
+  int textMode;
+} AppState;
 
 // clang-format off
-static Config config = {
+static AppState state = {
   .minDist = 2.0f,
   .maxDist = 10.0f,
   .dotRadius = 1.0f,
@@ -38,52 +41,66 @@ static Config config = {
   .gamma = 3.0f,
   .invert = 0,
   .bg = 0xffffff,
-  .fg = 0x0000ff
+  .fg = 0x0000ff,
+  .textMode = 0
 };
 // clang-format on
 
-static float disc_gen(CT_Quadtree *t, CT_Circle2f *disc, void *state) {
+static float disc_gen(CT_Quadtree *t, CT_Circle2f *disc, void *s) {
   ct_set2fxy(&disc->pos, ct_rand_normpos() * t->root.w,
              ct_rand_normpos() * t->root.h);
-  size_t x               = (size_t)disc->pos.x;
-  size_t y               = (size_t)disc->pos.y;
-  Config *config         = (Config *)state;
-  float tt               = (float)config->image[y * config->width + x] / 255.f;
-  if (config->invert) tt = 1.0f - tt;
+  size_t x              = (size_t)disc->pos.x;
+  size_t y              = (size_t)disc->pos.y;
+  AppState *state       = (AppState *)s;
+  float tt              = (float)state->image[y * state->width + x] / 255.f;
+  if (state->invert) tt = 1.0f - tt;
   return disc->r =
-             ct_mixf(config->minDist, config->maxDist, powf(tt, config->gamma));
+             ct_mixf(state->minDist, state->maxDist, powf(tt, state->gamma));
 }
 
-static int poisson_svg(const Config *config) {
-  CT_Quadtree t;
-  CT_MPool vpool;
-  ct_qtree_init(&t, 0, 0, config->width, config->height, 0x4000);
-  ct_mpool_init(&vpool, 0x4000, sizeof(CT_Vec2f));
-  ct_svg_start_doc(stdout, ct_svg_attribs(1, 2, SVG_INT("width", config->width),
-                                          SVG_INT("height", config->height)));
-  ct_svg_write_rect(stdout, 0, 0, config->width, config->height,
-                    ct_svg_attribs(1, 1, SVG_HEX("fill", config->bg)));
+static int poisson_svg(AppState *state) {
+  ct_svg_start_doc(stdout, ct_svg_attribs(1, 2, SVG_INT("width", state->width),
+                                          SVG_INT("height", state->height)));
+  ct_svg_write_rect(stdout, 0, 0, state->width, state->height,
+                    ct_svg_attribs(1, 1, SVG_HEX("fill", state->bg)));
   ct_svg_start_group(stdout, ct_svg_attribs(1, 2, SVG_STR("stroke", "none"),
-                                            SVG_HEX("fill", config->fg)));
-  srand(time(0));
+                                            SVG_HEX("fill", state->fg)));
   size_t count  = 0;
   size_t failed = 0;
-  CT_Vec2f *p   = (CT_Vec2f *)ct_mpool_alloc(&vpool);
+  CT_Vec2f *p   = (CT_Vec2f *)ct_mpool_alloc(&state->vpool);
   while (1) {
-    if (!ct_poisson_sample2f_with(&t, disc_gen, (void *)config, 5, p)) {
-      ct_svg_write_circle(stdout, p->x, p->y, config->dotRadius, NULL);
-      p = (CT_Vec2f *)ct_mpool_alloc(&vpool);
+    if (!ct_poisson_sample2f_with(&state->tree, disc_gen, (void *)state, 5,
+                                  p)) {
+      ct_svg_write_circle(stdout, p->x, p->y, state->dotRadius, NULL);
+      p = (CT_Vec2f *)ct_mpool_alloc(&state->vpool);
       count++;
       failed = 0;
-    } else if (++failed > config->quality) {
+    } else if (++failed > state->quality) {
       break;
     }
   }
   ct_svg_end_group(stdout);
   ct_svg_end_doc(stdout);
   fprintf(stderr, "%zu points", count);
-  ct_mpool_free(&vpool);
-  ct_qtree_free(&t);
+  return 0;
+}
+
+static int poisson_txt(AppState *state) {
+  size_t count  = 0;
+  size_t failed = 0;
+  CT_Vec2f *p   = (CT_Vec2f *)ct_mpool_alloc(&state->vpool);
+  while (1) {
+    if (!ct_poisson_sample2f_with(&state->tree, disc_gen, (void *)state, 5,
+                                  p)) {
+      fprintf(stdout, "%1.3f,%1.3f\n", p->x, p->y);
+      p = (CT_Vec2f *)ct_mpool_alloc(&state->vpool);
+      count++;
+      failed = 0;
+    } else if (++failed > state->quality) {
+      break;
+    }
+  }
+  fprintf(stderr, "%zu points", count);
   return 0;
 }
 
@@ -91,86 +108,96 @@ static inline uint8_t luminance(uint8_t r, uint8_t g, uint8_t b) {
   return (uint8_t)(((uint32_t)r + r + r + b + g + g + g + g) >> 3);
 }
 
-static void grayscale_image(Config *config) {
-  size_t size   = config->width * config->height;
+static void grayscale_image(AppState *state) {
+  size_t size   = state->width * state->height;
   uint8_t *gray = malloc(size);
-  for (size_t i = 0, j = 0; i < size; i++, j += config->channels) {
+  for (size_t i = 0, j = 0; i < size; i++, j += state->channels) {
     gray[i] =
-        luminance(config->image[j], config->image[j + 1], config->image[j + 2]);
+        luminance(state->image[j], state->image[j + 1], state->image[j + 2]);
   }
-  stbi_image_free(config->image);
-  config->image = gray;
+  stbi_image_free(state->image);
+  state->image = gray;
 }
 
-static int convert(Config *config) {
+static int convert(AppState *state) {
   // clang-format on
-  config->image = stbi_load(config->path, &config->width, &config->height,
-                            &config->channels, 0);
-  if (config->image == NULL) {
-    fprintf(stderr, "error loading image: %s\n", config->path);
+  state->image = stbi_load(state->path, &state->width, &state->height,
+                           &state->channels, 0);
+  if (state->image == NULL) {
+    fprintf(stderr, "error loading image: %s\n", state->path);
     return 1;
   }
 
   fprintf(stderr, "min: %f, max: %f, r: %f, q: %u, fg: %06x, bg: %06x\n",
-          config->minDist, config->maxDist, config->dotRadius, config->quality,
-          config->fg, config->bg);
-  fprintf(stderr, "image size: %d x %d @ %d\n", config->width, config->height,
-          config->channels);
+          state->minDist, state->maxDist, state->dotRadius, state->quality,
+          state->fg, state->bg);
+  fprintf(stderr, "image size: %d x %d @ %d\n", state->width, state->height,
+          state->channels);
 
-  if (config->channels > 1) {
-    grayscale_image(config);
+  if (state->channels > 1) {
+    grayscale_image(state);
   }
 
-  return poisson_svg(config);
+  srand(time(0));
+  ct_qtree_init(&state->tree, 0, 0, state->width, state->height, 0x4000);
+  ct_mpool_init(&state->vpool, 0x4000, sizeof(CT_Vec2f));
+  int res = (state->textMode ? poisson_txt(state) : poisson_svg(state));
+  ct_mpool_free(&state->vpool);
+  ct_qtree_free(&state->tree);
+  return res;
 }
 
-static void usage(const Config *config) {
+static void usage(const AppState *state) {
   fprintf(stderr, "Usage:\tex-poisson [options] image [ > out.svg ]\n");
-  fprintf(stderr, "\t-b HEX\t\tbg color (default: %06x)\n", config->bg);
-  fprintf(stderr, "\t-f HEX\t\tfg color (default: %06x)\n", config->fg);
-  fprintf(stderr, "\t-g FLOAT\tgamma (default: %1.2f)\n", config->gamma);
+  fprintf(stderr, "\t-b HEX\t\tbg color (default: %06x)\n", state->bg);
+  fprintf(stderr, "\t-f HEX\t\tfg color (default: %06x)\n", state->fg);
+  fprintf(stderr, "\t-g FLOAT\tgamma (default: %1.2f)\n", state->gamma);
   fprintf(stderr, "\t-i\t\tinvert (also swaps fg/bg) (default: %s)\n",
-          config->invert ? "yes" : "no");
+          state->invert ? "yes" : "no");
   fprintf(stderr, "\t-m FLOAT\tmin distance (default: %1.2f)\n",
-          config->minDist);
+          state->minDist);
   fprintf(stderr, "\t-x FLOAT\tmax distance (default: %1.2f)\n",
-          config->maxDist);
+          state->maxDist);
   fprintf(stderr, "\t-r FLOAT\tdot radius (default: %1.2f)\n",
-          config->dotRadius);
-  fprintf(stderr, "\t-q INT\t\tquality (default: %u)\n", config->quality);
+          state->dotRadius);
+  fprintf(stderr, "\t-q INT\t\tquality (default: %u)\n", state->quality);
+  fprintf(stderr, "\t-t\t\toutput points as text only (default: no)\n");
 }
 
 int main(int argc, char **argv) {
   char c;
   uint8_t err = 0;
-  while (!err && (c = getopt(argc, argv, "b:f:g:im:q:r:x:")) != -1) {
+  while (!err && (c = getopt(argc, argv, "b:f:g:im:q:r:tx:")) != -1) {
     switch (c) {
       case 'm':
-        config.minDist = ct_parse_float(optarg, config.minDist);
+        state.minDist = ct_parse_float(optarg, state.minDist);
         break;
       case 'x':
-        config.maxDist = ct_parse_float(optarg, config.maxDist);
+        state.maxDist = ct_parse_float(optarg, state.maxDist);
         break;
       case 'r':
-        config.dotRadius = ct_parse_float(optarg, config.dotRadius);
+        state.dotRadius = ct_parse_float(optarg, state.dotRadius);
         break;
       case 'g':
-        config.gamma = ct_parse_float(optarg, config.gamma);
+        state.gamma = ct_parse_float(optarg, state.gamma);
         break;
       case 'q':
-        config.quality = (uint32_t)ct_parse_float(optarg, config.quality);
+        state.quality = (uint32_t)ct_parse_float(optarg, state.quality);
         break;
       case 'f':
-        config.fg = ct_parse_hex32(optarg, config.fg);
+        state.fg = ct_parse_hex32(optarg, state.fg);
         break;
       case 'b':
-        config.bg = ct_parse_hex32(optarg, config.bg);
+        state.bg = ct_parse_hex32(optarg, state.bg);
         break;
       case 'i':
-        config.invert = !config.invert;
-        config.fg ^= config.bg;
-        config.bg ^= config.fg;
-        config.fg ^= config.bg;
+        state.invert = !state.invert;
+        state.fg ^= state.bg;
+        state.bg ^= state.fg;
+        state.fg ^= state.bg;
+        break;
+      case 't':
+        state.textMode = 1;
         break;
       case '?':
         fprintf(stderr, "Option -%c requires an argument\n", optopt);
@@ -189,9 +216,9 @@ int main(int argc, char **argv) {
     err = 1;
   }
   if (err) {
-    usage(&config);
+    usage(&state);
     return 1;
   }
-  config.path = argv[optind];
-  return convert(&config);
+  state.path = argv[optind];
+  return convert(&state);
 }
