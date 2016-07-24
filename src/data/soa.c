4756085d2629d018d745c3415ed8ef00f79eca9c
Karsten Schmidt <k@postspectacular.com>
2016-07-24 17:20:09 +0100

    add immutable SOA math ops

--- src/data/soa.c
+++ src/data/soa.c
@@ -27,7 +27,7 @@ fail:
 }
 
 int ct_soa_init(CT_SOA *s, void **comps, size_t width, size_t num) {
-  CT_CHECK(comps, "comps is NULL", comps);
+  CT_CHECK(comps, "comps is NULL");
   s->comps = comps;
   s->width = width;
   s->num   = num;
@@ -102,22 +102,41 @@ CT_SOA_PROLOGUE2_IMM(div) {
 }
 CT_SOA_EPILOGUE_IMM;
 
-CT_SOA2_PROLOGUE3(dot2) {
-  out[i] = ax[i] * bx[i] + ay[i] * by[i];
+/// immutable SOA ops
+
+CT_SOA_PROLOGUE3(add) {
+  oo[j] = aa[j] + bb[j];
 }
 CT_SOA_EPILOGUE3;
 
-CT_SOA3_PROLOGUE3(dot3) {
-  out[i] = ax[i] * bx[i] + ay[i] * by[i] + az[i] * bz[i];
+CT_SOA_PROLOGUE3(sub) {
+  oo[j] = aa[j] - bb[j];
 }
 CT_SOA_EPILOGUE3;
+
+CT_SOA_PROLOGUE3(mul) {
+  oo[j] = aa[j] * bb[j];
+}
+CT_SOA_EPILOGUE3;
+
+CT_SOA_PROLOGUE3(div) {
+  oo[j] = aa[j] / bb[j];
+}
+CT_SOA_EPILOGUE3;
+
+//// 2D SOA ops
+
+CT_SOA2_PROLOGUE3(dot2) {
+  oo[i] = ax[i] * bx[i] + ay[i] * by[i];
+}
+CT_SOA_EPILOGUE3_ALT;
 
 CT_SOA2_PROLOGUE3(dist2) {
   ct_soa_vec dx = ax[i] - bx[i];
   ct_soa_vec dy = ay[i] - by[i];
-  out[i]        = CT_SOA_SQRT(dx * dx + dy * dy);
+  oo[i]        = CT_SOA_SQRT(dx * dx + dy * dy);
 }
-CT_SOA_EPILOGUE3;
+CT_SOA_EPILOGUE3_ALT;
 
 CT_SOA *ct_soa_normalize2f_imm(CT_SOA *a, float len) {
   size_t n       = a->num >> CT_SOA_WORD_SHIFT;
@@ -135,3 +154,10 @@ CT_SOA *ct_soa_normalize2f_imm(CT_SOA *a, float len) {
 fail:
   return NULL;
 }
+
+//// 3D SOA ops
+
+CT_SOA3_PROLOGUE3(dot3) {
+  oo[i] = ax[i] * bx[i] + ay[i] * by[i] + az[i] * bz[i];
+}
+CT_SOA_EPILOGUE3_ALT;
