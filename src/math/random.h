3dd3f82ea06b032cd8f40cec217821b3edab713b
Karsten Schmidt <k@postspectacular.com>
2016-08-06 18:19:57 +0100

    add smush_norm & smush_normpos

--- src/math/random.h
+++ src/math/random.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "cthing.h"
 
 typedef struct {
@@ -7,6 +9,11 @@ typedef struct {
 
 // https://gist.github.com/voidqk/d112165a26b45244a65298933c0349a4
 
+ct_inline void ct_smush_init(CT_Smush *rnd, uint32_t s) {
+  rnd->seed = s;
+  rnd->iter = 0;
+}
+
 ct_inline uint32_t ct_smush(CT_Smush *rnd) {
   const uint32_t m = 0x5bd1e995;
   const uint32_t k = rnd->iter++ * m;
@@ -14,7 +21,10 @@ ct_inline uint32_t ct_smush(CT_Smush *rnd) {
   return rnd->seed ^ (rnd->seed >> 13);
 }
 
-ct_inline void ct_smush_init(CT_Smush *rnd, uint32_t s) {
-  rnd->seed = s;
-  rnd->iter = 0;
+ct_inline float ct_smush_norm(CT_Smush *rnd) {
+  return (float)ct_smush(rnd) / (float)(UINT32_MAX >> 1) - 1.f;
+}
+
+ct_inline float ct_smush_normpos(CT_Smush *rnd) {
+  return (float)ct_smush(rnd) / (float)UINT32_MAX;
 }
