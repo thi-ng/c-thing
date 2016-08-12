16aa2392ad402a50ab3642630944edbb61364b3b
Karsten Schmidt <k@postspectacular.com>
2016-08-12 18:47:18 +0100

    update postprocess.js to take paths as args

--- postprocess.js
+++ postprocess.js
@@ -1,6 +1,6 @@
 fs=require('fs');
-src=fs.readFileSync('cthing.js','utf8');
+src=fs.readFileSync(process.argv[2],'utf8');
 src=src.replace(/_ct_\w+_\d+:[\w\$]+,?/g, '');
 src=src.replace(/\w+\._ct_\w+_\d+\=\w+._ct_\w+_\d+;/g, '');
 src=src.replace(/;\s{2,}/g,'\n');
-fs.writeFileSync('cthing.min.js', src, 'utf8');
+fs.writeFileSync(process.argv[3], src, 'utf8');
